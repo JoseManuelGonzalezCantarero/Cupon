4d310e6ba505b04b1bb5dc03bf04901e5c713a9b
manu <sefirot_cloud@hotmail.com>
2016-03-09 18:22:06 +0100

    Capítulo 6 y 7 hasta el punto 7.1

--- src/AppBundle/Controller/SitioController.php
+++ src/AppBundle/Controller/SitioController.php
@@ -4,13 +4,43 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class SitioController extends Controller
 {
     /**
+     * @param $ciudad
+     * @return RedirectResponse
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/{ciudad}", name="portada")
+     */
+    public function portadaAction($ciudad = null)
+    {
+        if($ciudad == null)
+        {
+            $ciudad = $this->getParameter('cupon.ciudad_por_defecto');
+            return new RedirectResponse($this->generateUrl('portada', array('ciudad' => $ciudad)));
+        }
+        $em = $this->getDoctrine()->getManager();
+        $oferta = $em->getRepository('AppBundle:Oferta')->findOneBy(array(
+            'ciudad' => $this->getParameter('cupon.ciudad_por_defecto'),
+            'fechaPublicacion' => new \DateTime('today')
+        ));
+        if(!$oferta)
+        {
+            throw $this->createNotFoundException(
+                'No se ha encontrado la oferta del día en la ciudad seleccionada'
+            );
+        }
+        return $this->render('portada.html.twig', array(
+            'oferta' => $oferta
+        ));
+    }
+
+    /**
      * @param $pagina
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/sitio/{pagina}/")
+     * @Route("/sitio/{pagina}/", name="paginaEstatica")
      */
     public function estaticaAction($pagina)
     {
