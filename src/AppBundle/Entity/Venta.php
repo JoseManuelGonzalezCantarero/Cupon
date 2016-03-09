f34b2d228990ab0d1a94d975a9dba37fa1728edc
manu <sefirot_cloud@hotmail.com>
2016-03-09 15:34:54 +0100

    Capítulo 5

---
+++ src/AppBundle/Entity/Venta.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Venta
+ *
+ * @ORM\Table(name="ventas")
+ * @ORM\Entity
+ */
+class Venta
+{
+    /** @ORM\Column(type="datetime") */
+    protected $fecha;
+
+    /**
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Oferta")
+     */
+    protected $oferta;
+
+    /**
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Usuario")
+     */
+    protected $usuario;
+
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+    }
+
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    public function setOferta(Oferta $oferta)
+    {
+        $this->oferta = $oferta;
+    }
+
+    public function getOferta()
+    {
+        return $this->oferta;
+    }
+
+    public function setUsuario(Usuario $usuario)
+    {
+        $this->usuario = $usuario;
+    }
+
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+}
