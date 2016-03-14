af6b49ee90d62ccc086d949eed1104de785b1a40
manu <sefirot_cloud@hotmail.com>
2016-03-14 13:45:51 +0100

    Capítulo 11 Finalizado

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,18 +1,121 @@
 <?php
-
 namespace AppBundle\Tests\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /** @test */
+    public function laPortadaSimpleRedirigeAUnaCiudad()
     {
         $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode(),
+            'La portada redirige a la portada de una ciudad (status 302)'
+        );
+    }
+    /** @test */
+    public function laPortadaSoloMuestraUnaOfertaActiva()
+    {
+        $client = static::createClient();
+        $client->followRedirects(true);
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $ofertasActivas = $crawler->filter(
+            'article section.descripcion a:contains("Comprar")'
+        )->count();
+
+        $this->assertEquals(1, $ofertasActivas,
+            'La portada muestra una única oferta activa que se puede comprar'
+        );
+    }
+
+    /** @test */
+    public function losUsuariosPuedenRegistrarseDesdeLaPortada()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $crawler = $client->followRedirect();
+
+        $numeroEnlacesRegistrarse = $crawler->filter(
+            'html:contains("Regístrate")'
+        )->count();
+
+        $this->assertGreaterThan(0, $numeroEnlacesRegistrarse,
+            'La portada muestra al menos un enlace o botón para registrarse'
+        );
+    }
+
+    /** @test */
+    public function losUsuariosAnonimosVenLaCiudadPorDefecto()
+    {
+        $client = static::createClient();
+        $client->followRedirects(true);
+
+        $crawler = $client->request('GET', '/');
+
+        $ciudadPorDefecto = $client->getContainer()->getParameter(
+            'cupon.ciudad_por_defecto'
+        );
+
+        $ciudadPortada = $crawler->filter(
+            'header nav select option[selected="selected"]'
+        )->attr('value');
+
+        $this->assertEquals($ciudadPorDefecto, $ciudadPortada,
+            'Los usuarios anónimos ven seleccionada la ciudad por defecto'
+        );
+    }
+
+//    /** @test */
+//    public function losUsuariosAnonimosDebenLoguearseParaPoderComprar()
+//    {
+//        $client = static::createClient();
+//        $client->request('GET', '/');
+//        $crawler = $client->followRedirect();
+//
+//        $comprar = $crawler->selectLink('Comprar')->link();
+//        $client->click($comprar);
+//        $crawler = $client->followRedirect();
+//
+//        $this->assertRegExp(
+//            '/.*\/usuario\/login_check/',
+//            $crawler->filter('article form')->attr('action'),
+//            'Después de pulsar el botón de comprar, el usuario anónimo
+//             ve el formulario de login'
+//        );
+//    }
+
+    /** @test */
+    public function laPortadaRequierePocasConsultasDeBaseDeDatos()
+    {
+        $client = static::createClient();
+        $client->enableProfiler();
+        $client->request('GET', '/');
+
+        if ($profiler = $client->getProfile()) {
+            $this->assertLessThan(
+                4,
+                count($profiler->getCollector('db')->getQueries()),
+                'La portada requiere menos de 4 consultas a la base de datos'
+            );
+        }
+    }
+
+    /** @test */
+    public function laPortadaSeGeneraMuyRapido()
+    {
+        $client = static::createClient();
+        $client->enableProfiler();
+        $client->request('GET', '/');
+
+        if ($profiler = $client->getProfile()) {
+            $this->assertLessThan(
+                500,
+                $profiler->getCollector('time')->getDuration(),
+                'La portada se genera en menos de medio segundo'
+            );
+        }
     }
 }
---
+++ src/AppBundle/Tests/Twig/AppExtensionTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace AppBundle\Tests\Twig;
+
+use AppBundle\Twig\AppExtension;
+
+class TwigExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDescuento()
+    {
+        $extension = new AppExtension();
+
+        $this->assertEquals('-', $extension->descuento(100, null),
+            'El descuento no puede ser null'
+        );
+        $this->assertEquals('-', $extension->descuento('a', 3),
+            'El precio debe ser un número'
+        );
+        $this->assertEquals('-', $extension->descuento(100, 'a'),
+            'El descuento debe ser un número'
+        );
+
+        $this->assertEquals('0%', $extension->descuento(10, 0),
+            'Un descuento de cero euros se muestra como 0%'
+        );
+        $this->assertEquals('-80%', $extension->descuento(2, 8),
+            'Si el precio de venta son 2 euros y el descuento sobre el precio
+             original son 8 euros, el descuento es -80%'
+        );
+        $this->assertEquals('-33%', $extension->descuento(10, 5),
+            'Si el precio de venta son 10 euros y el descuento sobre el precio
+             original son 5 euros, el descuento es -33%'
+        );
+        $this->assertEquals('-33.33%', $extension->descuento(10, 5, 2),
+            'Si el precio de venta son 10 euros y el descuento sobre el precio
+             original son 5 euros, el descuento es -33.33% con dos decimales'
+        );
+    }
+}
