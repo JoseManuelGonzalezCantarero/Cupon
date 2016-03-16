f19bb2e82fad41771a34e05ac3230a6cd033e3d2
manu <sefirot_cloud@hotmail.com>
2016-03-16 10:40:25 +0100

    Capítulo 16 Finalizado (update)

--- src/AppBundle/Controller/BackendController.php
+++ src/AppBundle/Controller/BackendController.php
@@ -2,11 +2,17 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class BackendController extends Controller
 {
+    /**
+     * @param $ciudad
+     * @return RedirectResponse
+     * @Route("/cambiar-a-{ciudad}/", name="backendCiudadCambiar")
+     */
     public function ciudadCambiarAction($ciudad)
     {
         $this->get('request')->getSession()->set('ciudad', $ciudad);
